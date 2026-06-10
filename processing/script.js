41920423e606db7560b5abda67037f0842f92dfb
Gustavo Desgin <149828510+EmptyCet@users.noreply.github.com>
2026-06-10 14:41:55 +0100

    Processing Canvas adaptada Mobile
    Página do processing adaptada para caber em mobile.
    (Ainda com comportamento estranho dado a rotação da canvas)

--- processing/script.js
+++ processing/script.js
@@ -19,39 +19,39 @@ function setup() {
 }
 
 
-function draw() {
-  // Clear the background
-  background(0);
+// function draw() {
+//   // Clear the background
+//   background(0);
 
-  // Loop through angles 0, 30, 60, 90 degrees
-  for (let angle=0; angle <= 90; angle += 30) {
-      // Save current coordinate system
-      push();                       
+//   // Loop through angles 0, 30, 60, 90 degrees
+//   for (let angle=0; angle <= 90; angle += 30) {
+//       // Save current coordinate system
+//       push();                       
 
-      // Translate to center of canvas and rotate by angle
-      translate(width/2, height/2);
-      rotate(angle);
+//       // Translate to center of canvas and rotate by angle
+//       translate(width/2, height/2);
+//       rotate(angle);
 
-      // Set color based on angle and draw line along x-axis
-      stroke(angle+100, 100, 100);
-      strokeWeight(5);
-      line(0, 0, 150, 0);
+//       // Set color based on angle and draw line along x-axis
+//       stroke(angle+100, 100, 100);
+//       strokeWeight(5);
+//       line(0, 0, 150, 0);
 
-      // Display the angle
-      strokeWeight(1);              
-      text(angle, 170, 0);
+//       // Display the angle
+//       strokeWeight(1);              
+//       text(angle, 170, 0);
 
-      // Restore coordinate system
-      pop();                        
-  }
+//       // Restore coordinate system
+//       pop();                        
+//   }
 
-  // Draw the animated line
-  translate(width/2, height/2);
-  rotate(frameCount);
-  stroke(255);
-  strokeWeight(5);
-  line(0, 0, 150, 0);
-}
+//   // Draw the animated line
+//   translate(width/2, height/2);
+//   rotate(frameCount);
+//   stroke(255);
+//   strokeWeight(5);
+//   line(0, 0, 150, 0);
+// }
 
 function setup() {
   createCanvas(710, 400, WEBGL);
